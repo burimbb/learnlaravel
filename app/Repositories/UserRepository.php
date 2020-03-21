0a87bae4c88e5c31c686571330781e004f397d5a
Burim Bushi <burimbushii@hotmail.com>
2020-03-21 21:18:11 +0100

    added method on userrepo

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,10 +6,19 @@ use App\User;
 
 class UserRepository implements UserRepositoryInterface
 {
+    /**
+     * Create new public function
+     */
+    public function insert($attributes)
+    {
+        \DB::table('users')->insert($attributes);
+    }
+
     public function getAll()
-    {               
+    {
         return User::all();
     }
+
     public function getStartsWith($attribute, $startsWith)
     {
         return User::where($attribute, 'LIKE', "{$startsWith}%")
